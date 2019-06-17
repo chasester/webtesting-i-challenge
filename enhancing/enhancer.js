17f5ece1bcc1a2798d690173c66ce044784349ce
Chase Wex <clapingmanly@hotmail.com>
2019-06-17 17:32:57 -0400

    Creaded basic enhancement code

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,21 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
-
 function succeed(item) {
-  return { ...item };
+
+  return { ...item, enhancement: Math.min(item.enhancement+1, 20) };
 }
 
 function fail(item) {
-  return { ...item };
+  return { ...item, duriblity: Math.max(item.duriblity - (item.enhancement >= 15 ? 10 : 5), 0), enhancement: item.enhancement - (item.enhancement > 16 ? 1 : 0) };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, duriblity: 100};
 }
 
 function get(item) {
-  return { ...item };
+  return `${item.name} ${item.enhancement > 0 ? `[+${item.enhancement}]`: ""}`
 }
